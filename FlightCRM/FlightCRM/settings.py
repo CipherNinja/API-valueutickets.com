56a35a9f6caf2c7fd80a5465650c7906b71c893d
Priyesh Pandey <91175201+CipherNinja@users.noreply.github.com>
2025-02-19 12:54:22 +0530

    DRF session authentication is removed from settings.py

--- FlightCRM/FlightCRM/settings.py
+++ FlightCRM/FlightCRM/settings.py
@@ -256,12 +256,12 @@ JAZZMIN_UI_TWEAKS = {
 }
 
 # DRF Configuration (if necessary)
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.BasicAuthentication',
+#         'rest_framework.authentication.SessionAuthentication',
+#     ],
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ],
+# }
