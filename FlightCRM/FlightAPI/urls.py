4a7adfd71b835ed7d775ba0e23177bf778de4aea
CypherNinja <priyesh.kumarjii@gmail.com>
2025-02-24 11:23:25 +0530

    API for roundtrip is available on api/v1/flight/search/roundtrip/

--- FlightCRM/FlightAPI/urls.py
+++ FlightCRM/FlightAPI/urls.py
@@ -9,6 +9,7 @@ router.register(r'airports', views.AirportViewSet)
 urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api/v1/flight/search/onewaytrip/',views.FlightOnewayTrip.as_view(),name="onewaytrip"),
+    path('api/v1/flight/search/roundtrip/',views.FlightRoundTrip.as_view(),name="roundtrip"),
     path('api/v2/flight/booking/', views.FlightBookingCreateView.as_view(), name='flight-booking-create'),
     path('api/email/auth/resp/<str:booking_id>/<str:email_id>/<str:customer_response>', views.CustomerResponseView.as_view(), name='customer_response'),
     path('api/login/', views.LoginView.as_view(), name='login'),
